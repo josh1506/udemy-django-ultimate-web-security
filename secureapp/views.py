d02d629eb016790c7698dcdb1a5399d39d850076
josh1506 <jmjabor@gmail.com>
2025-06-16 12:47:48 +0800

    Features Initialize Django app with basic templates

---
+++ secureapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
