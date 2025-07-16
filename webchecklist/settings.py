cc2c88661154f0de7eb29df39c4c758f9a1eb7e5
josh1506 <jmjabor@gmail.com>
2025-07-16 12:37:44 +0800

    Configure environment variables using django-environ and external .env file

--- webchecklist/settings.py
+++ webchecklist/settings.py
@@ -10,19 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Django environ
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wxz4szaqh7dj_o3w!-j&pcc^@s$-d&kcu7xbc_#b=j+ye0556d'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
